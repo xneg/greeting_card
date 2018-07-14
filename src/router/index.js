801d9bdb2ca13883531c5c50886943187b3d0de0
Xneg <xnegxneg@gmail.com>
2018-07-15 00:30:13 +0300

    Наконец удалось добавить svg, который будет относительно легко редактировать.

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import BallsLevel from '../views/BallsLevel.vue'
+import CongratulationView from '../views/CongratulationView.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'BallsLevel',
+    //   component: BallsLevel
+    // },
     {
       path: '/',
-      name: 'BallsLevel',
-      component: BallsLevel
-    }
+      name: 'CongratulationView',
+      component: CongratulationView
+    },
+
   ]
 })
---
+++ src/views/CongratulationView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <nadia/>
+    </div>
+</template>
+
+<script>
+import Nadia from '../components/Nadia'
+export default {
+    components: {
+        Nadia
+    }    
+}
+</script>
+
+<style scoped>
+    /* #div {
+        background-color:#6a7590ff;
+    } */
+</style>
